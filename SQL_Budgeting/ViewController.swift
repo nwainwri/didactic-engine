d7ccf2f1c3db882bb03156f55ad6fa6f754804b4
nwainwri <nathanwainwright@gmail.com>
2018-09-04 14:20:36 -0700

    setup project files

---
+++ SQL_Budgeting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SQL_Budgeting
+//
+//  Created by Nathan Wainwright on 2018-09-04.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
